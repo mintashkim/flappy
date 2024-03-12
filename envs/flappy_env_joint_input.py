4540445133250b0675c5e4635a4960bebc5c8ea5
mintashkim <mkim5117@gmail.com>
2024-03-12 01:16:06 -0700

    update

--- envs/flappy_env_joint_input.py
+++ envs/flappy_env_joint_input.py
@@ -1,15 +1,12 @@
 # Helpers
-import os
-import sys
+import os, sys
 sys.path.append('/Users/mintaekim/Desktop/HRL/Flappy/Integrated/Flappy_Integrated/flappy_v2')
 sys.path.append('/Users/mintaekim/Desktop/HRL/Flappy/Integrated/Flappy_Integrated/flappy_v2/envs')
 import numpy as np
-import jax.numpy as jnp
-from jax import jit
 from typing import Dict, Union
 from collections import deque
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import pandas as pd
 import time
 # Gym
 import gymnasium as gym
@@ -49,7 +46,7 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
         # xml_file: str = "../assets/Flappy_v8_FixedAxis.xml",
         xml_file: str = "../assets/Flappy_v8_JointInput.xml",
         # xml_file: str = "../assets/Flappy_v8_Base.xml",
-        frame_skip: int = 2,
+        frame_skip: int = 1,
         default_camera_config: Dict[str, Union[float, int]] = DEFAULT_CAMERA_CONFIG,
         reset_noise_scale: float = 0.01,
         **kwargs
@@ -60,18 +57,18 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
 
         # Frequency
         self.max_timesteps         = max_timesteps
-        self.timestep              = 0.0
+        self.timestep: int         = 0
         self.sim_freq              = self.sim.freq # NOTE: 2000Hz for hard coding
         # self.dt                    = 1.0 / self.sim_freq # NOTE: 1/2000s for hard coding
-        self.policy_freq           = 30 # NOTE: 30Hz but the real control frequency might not be exactly 30Hz because we round up the num_sims_per_env_step
+        self.policy_freq: float    = 30.0 # NOTE: 30Hz but the real control frequency might not be exactly 30Hz because we round up the num_sims_per_env_step
         self.num_sims_per_env_step = self.sim_freq // self.policy_freq # 2000//30 = 66
         self.secs_per_env_step     = self.num_sims_per_env_step / self.sim_freq # 66/2000 = 0.033s
-        self.policy_freq           = int(1.0/self.secs_per_env_step) # 1000/33 = 30Hz
+        self.policy_freq: int      = int(1.0/self.secs_per_env_step) # 1000/33 = 30Hz
 
         self.is_visual          = is_visual
         self.randomize          = randomize
         self.debug              = debug
-        self.is_plotting        = True
+        self.is_plotting_joint  = True
         self.traj_type          = traj_type
         self.noisy              = False
         self.randomize_dynamics = False # True to randomize dynamics
@@ -106,6 +103,7 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
 
         # MujocoEnv
         self.model = mj.MjModel.from_xml_path(xml_file)
+        self.model.opt.timestep = self.dt
         self.data = mj.MjData(self.model)
         self.body_list = ["Base","L1","L2","L3","L4","L5","L6","L7",
                           "L1R","L2R","L3R","L4R","L5R","L6R","L7R"]
@@ -113,7 +111,21 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
                            'J1R','J2R','J3R','J5R','J6R','J7R','J10R']
         self.bodyID_dic, self.jntID_dic, self.posID_dic, self.jvelID_dic = self.get_bodyIDs(self.body_list)
         self.jID_dic = self.get_jntIDs(self.joint_list)
-        
+        # Joint Input
+        self.Angle_data = pd.read_csv("../assets/JointAngleData.csv", header=None)
+        self.J5_m = self.Angle_data.loc[:,0]  # Mujoco reference joint angle (θ_5)
+        self.J6_m = self.Angle_data.loc[:,1]  # Mujoco reference joint angle (θ_6)
+        self.J5v_m = self.sim.flapping_freq/2 * self.Angle_data.loc[:,2]  # Mujoco reference joint angle velocity (θ_5_dot)
+        self.J6v_m = self.sim.flapping_freq/2 * self.Angle_data.loc[:,3]  # Mujoco reference joint angle velocity (θ_6_dot)
+        self.t_m = np.linspace(0, 1.0/self.sim.flapping_freq, num=len(self.J5_m))
+        # Record Early Stage
+        self.SimTime = []
+        self.ua_ = []
+        self.JointAng = [[],[]]
+        self.JointAng_ref = [[],[]]
+        self.JointVel = [[],[]]
+        self.JointVel_ref = [[],[]]
+
         utils.EzPickle.__init__(self, xml_file, frame_skip, reset_noise_scale, **kwargs)
         MujocoEnv.__init__(self, xml_file, frame_skip, observation_space=self.observation_space, default_camera_config=default_camera_config, **kwargs)
         
@@ -136,9 +148,9 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
 
     @property
     def dt(self):
-        # if self.is_aero: return 2e-5 * self.frame_skip
-        # else: return self.model.opt.timestep * self.frame_skip
-        return 5e-4
+        # NOTE: For some reason, this does not set model.opt.timestep
+        #       To do so, go to xml and set timestep=""
+        return 1e-3
 
     def _init_env(self):
         print("Environment created")
@@ -217,9 +229,6 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
         # post-process action
         if self.lpf_action: action_filtered = self.action_filter.filter(action)
         else: action_filtered = np.copy(action)
-        # action_filtered[0] = 0
-        # action_filtered[0] = -29.8451302
-        # action_filtered = action_normalized
 
         self.do_simulation(action_filtered, self.frame_skip)
         obs = self._get_obs()
@@ -233,6 +242,10 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
         terminated = self._terminated(obs)
         truncated = False
         
+        # Plot recorded data
+        if self.is_plotting_joint and self.timestep == 1000:
+            self._plot_joint()
+        
         return obs, reward, terminated, truncated, self.info
     
     def do_simulation(self, ctrl, n_frames) -> None:
@@ -241,9 +254,15 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
         self._step_mujoco_simulation(ctrl, n_frames)
 
     def _step_mujoco_simulation(self, ctrl, n_frames):
-        self.data.ctrl[:] = ctrl
 
-        # NOTE: Custom Aero
+        self.data.actuator("Motor1").ctrl[0] = ctrl[2]  # data.ctrl[1] # front
+        self.data.actuator("Motor2").ctrl[0] = ctrl[3]  # data.ctrl[2] # back
+        self.data.actuator("Motor3").ctrl[0] = ctrl[4]  # data.ctrl[3] # left
+        self.data.actuator("Motor4").ctrl[0] = ctrl[5]  # data.ctrl[4] # right
+        self.data.actuator("Motor5").ctrl[0] = ctrl[6]  # data.ctrl[5] # left H
+        self.data.actuator("Motor6").ctrl[0] = ctrl[7]  # data.ctrl[6] # right H
+
+        # NOTE: If Using Custom Aero
         if self.is_aero:
             self.xd, R_body = self._get_original_states()
             fa, ua, self.xd = aero(self.model, self.data, self.xa, self.xd, R_body)
@@ -254,37 +273,84 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
             # Integrate Aero States
             self.xa = self.xa + fa * self.dt
 
+        # Joint Input Data
+        _J5 = np.interp(np.round(self.data.time,3), self.t_m, self.J5_m, period=1.0/self.sim.flapping_freq)
+        _J6 = np.interp(np.round(self.data.time,3), self.t_m, self.J6_m, period=1.0/self.sim.flapping_freq)
+        J5_d = _J5 - np.deg2rad(11.345825599281223)  # convert to Mujoco Reference
+        J6_d = _J6 + np.deg2rad(27.45260202) - _J5
+        # Apply angles to Joints
+        self.data.actuator("J5_angle").ctrl[0] = J5_d
+        self.data.actuator("J6_angle").ctrl[0] = J6_d
+
         mj.mj_step(self.model, self.data, nstep=n_frames)
-        mj.mj_rnePostConstraint(self.model, self.data)
+
+        # NOTE: Record joint angles
+        J5 = self.data.qpos[self.posID_dic["L3"]] + np.deg2rad(11.345825599281223) # Get angle from mujoco
+        J6 = self.data.qpos[self.posID_dic["L7"]] - np.deg2rad(27.45260202) + J5
+        self.SimTime.append(np.round((self.timestep + 1) * self.dt, 3))
+        self.JointAng_ref[0].append(_J5)
+        self.JointAng_ref[1].append(_J6)
+        J5v_d = np.interp(self.data.time, self.t_m, self.J5v_m, period=1.0 / self.sim.flapping_freq) # Get velocity just for comparison
+        J6v_d = np.interp(self.data.time, self.t_m, self.J6v_m, period=1.0 / self.sim.flapping_freq)
+        self.JointVel_ref[0].append(J5v_d)
+        self.JointVel_ref[1].append(J6v_d)
+        self.JointAng[0].append(J5)
+        self.JointAng[1].append(J6)
+        self.JointVel[0].append(self.data.qvel[self.jvelID_dic["L3"]])
+        self.JointVel[1].append(self.data.qvel[self.jvelID_dic["L7"]])
+
+    def _plot_joint(self):
+        plt.figure()
+        plt.subplot(2,1,1)
+        plt.title("Joint Angle (MuJoCo vs. Matlab)")
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointAng_ref[0][100:]), 'b--', label='J5 ref')
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointAng[0][100:]), 'b', label='J5 real')
+        plt.xlabel('Time, t (s)')
+        plt.ylabel('J5 Angle, θ_5 (deg)')
+        plt.legend()
+
+        plt.subplot(2,1,2)
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointAng_ref[1][100:]), 'g--', label='J6 ref')
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointAng[1][100:]), 'g', label='J6 real')
+        plt.xlabel('Time, t (s)')
+        plt.ylabel('J6 Angle, θ_6 (deg)')
+        plt.legend()
+
+        plt.figure()
+        plt.subplot(2,1,1)
+        plt.title("Joint Angle Velocity (MuJoCo vs. Matlab)")
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointVel_ref[0][100:]), 'b--', label='J5 ref')
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointVel[0][100:]), 'b', label='J5 real')
+        plt.xlabel('Time, t (s)')
+        plt.ylabel('J5 Speed, θ_5 (deg/s)')
+        # plt.ylim([-2000,2000])
+        plt.legend()
+
+        plt.subplot(2,1,2)
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointVel_ref[1][100:]), 'g--', label='J6 ref')
+        plt.plot(self.SimTime[100:], np.rad2deg(self.JointVel[1][100:]), 'g', label='J6 real')
+        plt.xlabel('Time, t (s)')
+        plt.ylabel('J6 Speed, θ_6 (deg/s)')
+        # plt.ylim([-3000,3000])
+        plt.legend()
+        plt.show()
 
     # NOTE: For aero()
     def _get_original_states(self):
-        # Takes mujoco states vectors and converts to MATLAB states vectors defined in func_eom
-        qpos = self.data.qpos
-        qvel = self.data.qvel     
-        N = len(qpos)
+        xd = np.array([0.0] * 22)
+        xd[0] = self.data.qpos[self.posID_dic["L3"]] + np.deg2rad(11.345825599281223)  # xd[0:1] left wing angles [shoulder, elbow]
+        xd[1] = self.data.qpos[self.posID_dic["L7"]] - np.deg2rad(27.45260202) + xd[0]
+        xd[2:5] = self.data.sensordata[0:3]  # inertial position (x, y, z)
+        xd[5] = self.data.qvel[self.jvelID_dic["L3"]]  # Joint 5 velocity
+        xd[6] = self.data.qvel[self.jvelID_dic["L7"]]  # Joint 6 velocity
+        xd[7:10] = self.data.sensordata[7:10]  # Inertial Frame Linear Velocity
+        xd[10:13] = self.data.sensordata[10:13]  # Body Frame Angular velocity
 
-        xd = np.zeros(22)
-        xd[0] = qpos[self.posID_dic["L5"]]
-        xd[1] = qpos[self.posID_dic["L6"]]
-        if N == 21:
-            xd[2:5] = qpos[0:3]
-        else:
-            xd[2:5] = np.array([0,0,0.5])
-
-        xd[5] = qvel[self.jvelID_dic["L5"]]
-        xd[6] = qvel[self.jvelID_dic["L6"]]
-        
-        if N == 21:
-            xd[7:10] = qvel[0:3]
-            xd[10:13] = qvel[3:6]
-        else:
-            xd[7:10] = np.zeros(3)
-            xd[10:13] = np.zeros(3)
-
-        R_B = R_body(self.model, self.data)
-        xd[13:23] = np.transpose(R_B).flatten()
-        return xd, R_B
+        if np.linalg.norm(self.data.sensordata[3:7]) == 0:
+            self.data.sensordata[3:7] = [1,0,0,0]
+        R_body = quat2rot(self.data.sensordata[3:7])
+        xd[13:23] = list(np.transpose(R_body).flatten())
+        return xd, R_body
 
     def _update_data(self, step=True):
         # NOTE: Need to be modifid to obs states and ground truth states 
@@ -303,7 +369,7 @@ class FlappyEnv(MujocoEnv, utils.EzPickle):
         w_velocity         = 1.0
         w_angular_velocity = 5.0
         w_orientation      = 10.0
-        w_input            = 20.0
+        w_input            = 5.0
         w_delta_act        = 0.1
 
         reward_weights = np.array([w_position, w_velocity, w_angular_velocity, w_orientation, w_input, w_delta_act])
